8f3283e44e6c5bc4d0e4202eeb0042e743e2cca2
Edgar <rbracing2014@gmail.com>
2017-04-01 18:23:25 +0200

    VERSION 0.0.04.01

---
+++ app/src/main/java/com/enav/cazaunchollo/cazaunchollo/OfferDescriptionFragment.java
@@ -0,0 +1,34 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.enav.cazaunchollo.cazaunchollo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class OfferDescriptionFragment extends Fragment {
+    private LinearLayout mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRootView = (LinearLayout) inflater.inflate(R.layout.fragment_page, container, false);
+        return mRootView;
+    }
+
+
+
+
+    public static Fragment newInstance() {
+        return new OfferDescriptionFragment();
+    }
+
+}
--- app/src/main/java/com/enav/cazaunchollo/cazaunchollo/OfferScrollingActivity.java
+++ app/src/main/java/com/enav/cazaunchollo/cazaunchollo/OfferScrollingActivity.java
@@ -1,29 +1,92 @@
 package com.enav.cazaunchollo.cazaunchollo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
-public class OfferScrollingActivity extends AppCompatActivity {
+public class OfferScrollingActivity extends AppCompatActivity
+implements AppBarLayout.OnOffsetChangedListener {
+
+        private int mMaxScrollSize;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_offer_scrolling);
+
+            TabLayout tabLayout = (TabLayout) findViewById(R.id.materialup_tabs);
+            ViewPager viewPager  = (ViewPager) findViewById(R.id.materialup_viewpager);
+            AppBarLayout appbarLayout = (AppBarLayout) findViewById(R.id.materialup_appbar);
+
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.materialup_toolbar);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+
+            toolbar.setTitle("PRUEBA");
+
+            appbarLayout.addOnOffsetChangedListener(this);
+            mMaxScrollSize = appbarLayout.getTotalScrollRange();
+
+            viewPager.setAdapter(new TabsAdapter(getSupportFragmentManager()));
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
+    public static void start(Context c) {
+        c.startActivity(new Intent(c, OfferScrollingActivity.class));
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_offer_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+        if (mMaxScrollSize == 0)
+            mMaxScrollSize = appBarLayout.getTotalScrollRange();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        int percentage = (Math.abs(i)) * 100 / mMaxScrollSize;
+
     }
+
+    class TabsAdapter extends FragmentPagerAdapter {
+        public TabsAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch(i) {
+                case 0: return OfferDescriptionFragment.newInstance();
+                case 1: return OfferCommentsFragment.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case 0: return "Descripcion";
+                case 1: return "Comentarios";
+            }
+            return "";
+        }
+    }
+
 }
